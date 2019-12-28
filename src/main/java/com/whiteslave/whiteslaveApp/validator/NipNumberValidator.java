acb7d798addd0e86adc955af64fc5029ce82aeb8
Maciej Wojcik <maciek@testaarosa.pl>
2019-12-28 12:37:52 +0100

    add date validator

--- src/main/java/com/whiteslave/whiteslaveApp/validator/NipNumberValidator.java
+++ src/main/java/com/whiteslave/whiteslaveApp/validator/NipNumberValidator.java
@@ -1,14 +1,12 @@
 package com.whiteslave.whiteslaveApp.validator;
 
 import com.google.common.collect.ImmutableMap;
-import org.apache.commons.collections.map.UnmodifiableMap;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.util.HashMap;
 import java.util.Map;
 
-public class NipNumberValidator extends ParamsSharedValidator implements ConstraintValidator<NipNumber, String> {
+public class NipNumberValidator extends GovNumbersSharedValidator implements ConstraintValidator<NipNumber, String> {
 
     private final static String LENGTH_MESSAGE = "NIP number has incorrect length. Required 10 characters => ";
     private final static String DIGIT_MESSAGE = "NIP number contains illegal characters. Only digits required=> ";
@@ -28,107 +26,7 @@ public class NipNumberValidator extends ParamsSharedValidator implements Constra
                         "DIGIT_MESSAGE", DIGIT_MESSAGE,
                         "CHECK_SUM_MESSAGE", CHECK_SUM_MESSAGE);
         return paramsValidateDistributor(nips, constraintValidatorContext, messageMap, NIP_LENGTH);
-//        boolean isNip = false;
-//        if (nips.contains(NIP_SEPARATOR)) {
-//            for (String nipNumber : nips.split(NIP_SEPARATOR)) {
-//                if (!isNipNumberValid(nipNumber)) {
-//                    isNip = false;
-//                    break;
-//                }
-//                isNip = true;
-//            }
-//        } else {
-//            isNip = isNipNumberValid(nips);
-//        }
-//        return isNip;
     }
-//
-//    private boolean isNipNumberValid(String nip) {
-//        if (!lengthValidation(NIP_LENGTH, nip, constraintValidatorContext)) {
-//            prepareMessage(LENGTH_MESSAGE + nip, constraintValidatorContext);
-//            return false;
-//        }
-//        if (!digitValidation(nip, constraintValidatorContext)) {
-//            prepareMessage(DIGIT_MESSAGE + nip, constraintValidatorContext);
-//            return false;
-//        }
-//        if (!checksumValidation(nip)) {
-//            prepareMessage(CHECK_SUM_MESSAGE + nip, constraintValidatorContext);
-//            return false;
-//        }
-//        return true;
-//
-//        if (lengthValidation(nip, constraintValidatorContext)) {
-//            if (digitValidation(nip, constraintValidatorContext)) {
-//                if (isNipNumbertValid(nip, constraintValidatorContext)) {
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-
-//    private boolean lengthValidation(String nip, ConstraintValidatorContext constraintValidatorContext) {
-//        if (nip.length() > 0 & nip.length() <= 26) {
-//            return true;
-//        } else {
-//            constraintValidatorContext
-//                    .buildConstraintViolationWithTemplate("NIP number has incorrect length. Required 26 characters => " + nip)
-//                    .addConstraintViolation();
-//            return false;
-//        }
-//    }
-//
-//    private boolean digitValidation(String nip, ConstraintValidatorContext constraintValidatorContext) {
-//        if (nip.matches(DIGIT_REGEX)) {
-//            return true;
-//        } else {
-//            constraintValidatorContext
-//                    .buildConstraintViolationWithTemplate("NIP number contains illegal characters. Only digits required=> " + nip)
-//                    .addConstraintViolation();
-//            return false;
-//        }
-//    }
-
-//    private boolean isNipValid(String nip, ConstraintValidatorContext constraintValidatorContext) {
-////        if (nip.length() != 10) {
-////            return false;
-////        }
-//        try {
-//            int sum = 0;
-//            for (int i = 0; i < WEIGHT.length; ++i) {
-//                sum += Integer.parseInt(nip.substring(i, i + 1)) * WEIGHT[i];
-//            }
-//            boolean isNipGit = (sum % 11) == Integer.parseInt(nip.substring(9, 10));
-//            if (isNipGit) {
-//                return isNipGit;
-//            } else {
-//                constraintValidatorContext
-//                        .buildConstraintViolationWithTemplate("NIP number is not valid => " + nip)
-//                        .addConstraintViolation();
-//                return isNipGit;
-//            }
-//        } catch (NumberFormatException e) {
-//            return false;
-//        }
-//    }
-
-//    @Override
-//    boolean isParamValid(String nip) {
-//        if (!lengthValidation(NIP_LENGTH, nip, constraintValidatorContext)) {
-//            prepareMessage(LENGTH_MESSAGE + nip, constraintValidatorContext);
-//            return false;
-//        }
-//        if (!digitValidation(nip, constraintValidatorContext)) {
-//            prepareMessage(DIGIT_MESSAGE + nip, constraintValidatorContext);
-//            return false;
-//        }
-//        if (!checksumValidation(nip)) {
-//            prepareMessage(CHECK_SUM_MESSAGE + nip, constraintValidatorContext);
-//            return false;
-//        }
-//        return true;
-//    }
 
     @Override
     boolean checksumValidation(String param) {
--- src/main/java/com/whiteslave/whiteslaveApp/validator/RegonNumberValidator.java
+++ src/main/java/com/whiteslave/whiteslaveApp/validator/RegonNumberValidator.java
@@ -6,12 +6,10 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Map;
 import java.util.Optional;
-import java.util.OptionalLong;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-public class RegonNumberValidator extends ParamsSharedValidator implements ConstraintValidator<RegonNumber, String> {
+public class RegonNumberValidator extends GovNumbersSharedValidator implements ConstraintValidator<RegonNumber, String> {
     private final static int[] WEIGHT_9_DIGIT = new int[]{8, 9, 2, 3, 4, 5, 6, 7};
     private final static int[] WEIGHT_14_DIGIT = new int[]{2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8};
     private final static String LENGTH_MESSAGE = "Regon number has incorrect length. Required 9 or 14 characters => ";
@@ -37,15 +35,15 @@ public class RegonNumberValidator extends ParamsSharedValidator implements Const
     @Override
     boolean checksumValidation(String param) {
         if (param.length() == SHORT_REGON_LENGTH) {
-            return checkSum9(param);
+            return checkSum9_14(param, WEIGHT_9_DIGIT);
         }
-        return checkSum14(param);
+        return checkSum9_14(param,WEIGHT_14_DIGIT);
     }
 
-    private boolean checkSum9(String regon9) {
+    private boolean checkSum9_14(String regon9, int[] weight) {
         AtomicInteger sum = new AtomicInteger();
-        IntStream.range(0, WEIGHT_9_DIGIT.length).forEach(i ->
-                sum.addAndGet(WEIGHT_9_DIGIT[i] *
+        IntStream.range(0, weight.length).forEach(i ->
+                sum.addAndGet(weight[i] *
                         Integer.parseInt(String.valueOf(regon9.charAt(i)))));
         return Optional.of(sum.get())
                 .map(s -> s % 11)
@@ -55,22 +53,38 @@ public class RegonNumberValidator extends ParamsSharedValidator implements Const
                     }
                     return s == Integer.parseInt(regon9.substring(regon9.length() - 1));
                 }).orElse(false);
+
     }
 
-    private boolean checkSum14(String regon14) {
-        AtomicInteger sum = new AtomicInteger();
-        IntStream.range(0, WEIGHT_14_DIGIT.length).forEach(i ->
-                sum.addAndGet(WEIGHT_14_DIGIT[i] *
-                        Integer.parseInt(String.valueOf(regon14.charAt(i)))));
-        return Optional.of(sum.get())
-                .map(s -> s % 11)
-                .map(s -> {
-                    if(s == 10) {
-                        s = 0;
-                    }
-                    return s == Integer.parseInt(regon14.substring(regon14.length() - 1));
-                })
-                .orElse(false);
-    }
+//    private boolean checkSum9(String regon9) {
+//        AtomicInteger sum = new AtomicInteger();
+//        IntStream.range(0, WEIGHT_9_DIGIT.length).forEach(i ->
+//                sum.addAndGet(WEIGHT_9_DIGIT[i] *
+//                        Integer.parseInt(String.valueOf(regon9.charAt(i)))));
+//        return Optional.of(sum.get())
+//                .map(s -> s % 11)
+//                .map(s -> {
+//                    if (s == 10) {
+//                        s = 0;
+//                    }
+//                    return s==Integer.parseInt(regon9.substring(regon9.length() - 1));
+//                }).orElse(false);
+//    }
+//
+//    private boolean checkSum14(String regon14) {
+//        AtomicInteger sum = new AtomicInteger();
+//        IntStream.range(0, WEIGHT_14_DIGIT.length).forEach(i ->
+//                sum.addAndGet(WEIGHT_14_DIGIT[i] *
+//                        Integer.parseInt(String.valueOf(regon14.charAt(i)))));
+//        return Optional.of(sum.get())
+//                .map(s -> s % 11)
+//                .map(s -> {
+//                    if(s == 10) {
+//                        s = 0;
+//                    }
+//                    return s == Integer.parseInt(regon14.substring(regon14.length() - 1));
+//                })
+//                .orElse(false);
+//    }
 }
 
