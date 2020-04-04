46f70457e38c1dc9db955a6f45ea35d67649eb64
Maciej Wojcik <maciek@testaarosa.pl>
2020-04-04 13:10:28 +0200

    ech, just one char

--- src/main/java/com/whiteslave/whiteslaveApp/archiveReport/archReportQuery/ArchReportQueryDto.java
+++ src/main/java/com/whiteslave/whiteslaveApp/archiveReport/archReportQuery/ArchReportQueryDto.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-4@Setter
+@Setter
 public class ArchReportQueryDto implements Serializable {
 
     private Long id;
