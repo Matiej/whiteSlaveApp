b81419e25872686d2baeca6bee50b8a894281449
Maciej Wojcik <maciek@testaarosa.pl>
2020-07-16 23:56:52 +0200

    implementation for custom httpheaders for exception and for ok reposnse
    (cherry picked from commit a9719e517e3ebe92c0f85be909fc3f928874257d)

--- src/main/java/com/whiteslave/whiteslaveApp/config/feignConfiguration/FeignConfiguration.java
+++ src/main/java/com/whiteslave/whiteslaveApp/config/feignConfiguration/FeignConfiguration.java
@@ -1,23 +1,10 @@
 package com.whiteslave.whiteslaveApp.config.feignConfiguration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.whiteslave.whiteslaveApp.govRequestReport.client.MfGovErrorDecoder;
-import feign.Feign;
 import feign.Logger;
-import feign.codec.Decoder;
 import feign.codec.ErrorDecoder;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
-import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 //@EnableFeignClients
